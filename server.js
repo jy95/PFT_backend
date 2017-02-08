96d8ed4f638ce68137e99d72923af0876c5f1640
Jacques <jacques.yakoub@gmail.com>
2017-02-08 11:53:21 +0100

    Code d'erreur FIX

--- server.js
+++ server.js
@@ -22,28 +22,16 @@ app.use('/', routes);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     let err = new Error('Not Found');
-    err.status = 404;
+    err.statusCode = 404;
     next(err);
 });
 
 // error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status( err.code || 500 )
-            .json({
-                status: 'error',
-                message: err
-            });
-    });
-}
 
 // production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500)
+    res.status(err.statusCode || 500)
         .json({
             status: 'error',
             message: err.message
